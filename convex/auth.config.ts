db06a9acdc185718799f2db8ebd2fefd5b7ecbcd
Tiger Abrodi <tigerabrodi@gmail.com>
2026-03-31 20:52:09 +0800

    update versions

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: process.env.CONVEX_SITE_URL,
+      domain: (process as { env: Record<string, string | undefined> }).env.CONVEX_SITE_URL,
       applicationID: "convex",
     },
   ],
